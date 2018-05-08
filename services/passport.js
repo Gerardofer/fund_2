ca2a3cee264b2fb3e3ff934c028bcb3d581b72bf
Gerardo Fernandez <gerardojose13@gmail.com>
2018-05-07 23:58:06 -0700

    adds new promise syntax

--- services/passport.js
+++ services/passport.js
@@ -23,17 +23,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          console.log("user already in the database");
-          console.log(existingUser);
-          done(null, existingUser);
-        }
-        new User({ googleId: profile.id }).save().then(user => {
-          done(null, user);
-        });
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        console.log("user already in the database");
+        console.log(existingUser);
+        done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
