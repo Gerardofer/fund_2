81d06dd4b6672fdd36f4248694c3fb46a2d89b25
Gerardo Fernandez <gerardojose13@gmail.com>
2018-05-13 00:35:24 -0700

    adds files with redux and redux-thunk

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 import Header from "./Header";
 
 const Home = () => <h1>This is home</h1>;
@@ -7,20 +9,26 @@ const Landing = () => <h1>This is the Landing</h1>;
 const Projects = () => <h1>This is the projects page</h1>;
 const ProjectsNew = () => <h1>Create a new Project</h1>;
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <Router>
-        <div>
-          <Route exact path="/" component={Home} />
-          <Route path="/landing" component={Landing} />
-          <Route exact path="/projects" component={Projects} />
-          <Route path="/projects/new" component={ProjectsNew} />
-        </div>
-      </Router>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div>
+        <Header />
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/landing" component={Landing} />
+            <Route exact path="/projects" component={Projects} />
+            <Route path="/projects/new" component={ProjectsNew} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
